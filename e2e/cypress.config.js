625f6ec3857252b2d7a28951018a4452da815ec5
Lauracs13 <97996208+Lauracs13@users.noreply.github.com>
2024-06-03 18:58:38 -0500

    modify config file

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://bookstorewebapp.azurewebsites.net/",
   },
+  reporter: "junit", // Specify the reporter
+  reporterOptions: {
+    mochaFile: "results/TEST-[hash].xml", // Specify the mocha file path
+  },
 });
 
-require('@applitools/eyes-cypress')(module);
+require("@applitools/eyes-cypress")(module);
